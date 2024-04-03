14a93545b1905131c99cb1fbfd38c643d931e530
ElianCastro <eliancastro98@gmail.com>
2024-04-03 20:58:40 -0300

    primer

---
+++ sumasSucesivas.go
@@ -0,0 +1,41 @@
+package main
+
+impo
+import "fmt"
+
+func producto(a, b int) int {
+    resultado := 
+    resultado :=
+
+    
+0
+    for i := 0; i < b; i++ {
+        resultado += a
+    }
+    
+        resultado += a
+    }
+   
+
+        resultado +
+
+     
+return resultado
+}
+
+
+}
+
+fun
+func main() {
+  
+    a := 
+    a 
+5
+    b := 
+    b
+3
+    fmt.Printf(
+    fm
+"El producto de %d y %d es %d\n", a, b, producto(a, b))
+}
