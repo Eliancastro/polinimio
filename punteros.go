fde126a174cff9c5434f9456c6b7d6deabf67027
ElianCastro <eliancastro98@gmail.com>
2024-04-03 21:22:47 -0300

    intercambio

---
+++ punteros.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func Swap(px, py *int) {
+    temp := *px
+    *px = *py
+    *py = temp
+}
+
+
+    temp := *px
+    *px = *py
+    *py = 
+
+    temp := *px
+    *
+
+    
+func main() {
+    // Ejemplo de uso
+    x := 5
+    y := 10
+
+    fmt.Println(
+
+    fmt.P
+"Antes del intercambio:")
+    fmt.Println(
+    fmt.Prin
+"x:", x)
+    fmt.Println(
+    fmt.Pri
+"y:", y)
+
+    Swap(&x, &y)
+
+    fmt.Println(
+
+    Swap(&x, &y)
+
+    fmt.Println
+
+
+    Swap(&x, &y)
+
+    Swap
+"Después del intercambio:")
+    fmt.Println("x:", x)
+    fmt.Println(
+    fmt.P
+"y:", y)
+}
