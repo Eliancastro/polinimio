174a02a25e45f6a3c6f7a549a99f77681500a80d
ElianCastro <eliancastro98@gmail.com>
2024-04-03 21:09:40 -0300

    suma

---
+++ sumaArreglo.go
@@ -0,0 +1,39 @@
+package main
+
+impor
+import "fmt"
+
+func sumaArreglo(arreglo []int) int {
+    suma := 
+    suma :=
+0
+    for _, valor := range arreglo {
+        suma += valor
+    }
+    
+        suma += valor
+    }
+    retu
+
+        suma += valor
+    }
+
+        suma += v
+
+      
+return suma
+}
+
+
+}
+
+f
+func main() {
+    // Ejemplo de uso
+    arreglo := []
+   
+int{1, 2, 3, 4, 5}
+    fmt.Printf(
+    fmt.Prin
+"La suma de los elementos del arreglo es: %d\n", sumaArreglo(arreglo))
+}
